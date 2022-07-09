685ae846b97ae53776173c51cc9b9a4d2e39a25d
iamgirya <iamgirya@yandex.ru>
2022-07-09 10:12:22 +0300

    Сохранение/Загрузка модели

--- main.py
+++ main.py
@@ -101,9 +101,67 @@ def recomend_to_user_with_merchants(user_id):
 
     return json.dumps(recomended_products)
 
+def saveModel(model):
+    convertedUF = []
+    for x in list(model.user_factors):
+        tmp = []
+        for y in list(x):
+            tmp.append(float(y))
+        convertedUF.append(tmp)
+    convertedIF = []
+    for x in list(model.item_factors):
+        tmp = []
+        for y in list(x):
+            tmp.append(float(y))
+        convertedIF.append(tmp)
+    mapOfModel = {'user_factors':convertedUF,'item_factors':convertedIF,'factors':model.factors,
+    'regularization':model.regularization,'iterations':model.iterations,'num_threads':model.num_threads,
+    'shapeU0':model.user_factors.shape[0],'shapeU1':model.user_factors.shape[1],
+    'shapeI0':model.item_factors.shape[0],'shapeI1':model.item_factors.shape[1],}
+    fp = open('savedModel.txt', 'w')
+    fp.write(json.dumps(mapOfModel))
+    fp.close()
+
+def loadModel():
+    fp = open('savedModel.txt', 'r')
+    mapOfModel = json.load(fp)
+    model = AlternatingLeastSquares(factors = mapOfModel['factors'], regularization = mapOfModel['regularization'], iterations = mapOfModel['iterations'], num_threads = mapOfModel['num_threads'])
+    
+    tmp = np.ndarray(shape = (mapOfModel['shapeU0'],mapOfModel['shapeU1']), dtype = np.float32)
+    for x in range(len(mapOfModel['user_factors'])):
+        tmp1 = np.ndarray( shape = (mapOfModel['shapeU1'],), dtype = np.float32 )
+        for y in range(len(mapOfModel['user_factors'][x])):
+            tmp1[y] = (np.float32(mapOfModel['user_factors'][x][y]))
+        tmp[x] = tmp1
+    model.user_factors = tmp
+
+    tmp = np.ndarray(shape = (mapOfModel['shapeI0'],mapOfModel['shapeI1']), dtype = np.float32)
+    for x in range(len(mapOfModel['item_factors'])):
+        tmp1 = np.ndarray( shape = (mapOfModel['shapeI1'],), dtype = np.float32 )
+        for y in range(len(mapOfModel['item_factors'][x])):
+            tmp1[y] = (np.float32(mapOfModel['item_factors'][x][y]))
+        tmp[x] = tmp1
+    model.item_factors = tmp
+
+    fp.close()
+    return model
+
 users, products, merchants = load_data()
 matrix = construct_matrix()
 data_matrix = transform_matrix_to_csr_matrix()
 model.fit(2 * data_matrix)
+
+# fp = open('1.txt', 'w')
+# fp.write(json.dumps(recomend_to_user_with_merchants(2217)))
+# fp.close()
+
+# saveModel(model)
+# model = None
+
+# model = loadModel() 
+
+# fp = open('2.txt', 'w')
+# fp.write(json.dumps(recomend_to_user_with_merchants(2217)))
+# fp.close()
 
 #print(recomend_to_user_with_merchants(2217))
