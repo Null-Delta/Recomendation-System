e672309f6e538cac4501e2e76ea312e08189693b
iamgirya <iamgirya@yandex.ru>
2022-07-09 03:26:58 +0300

    Метрика для обычного топа

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from array import array
+from collections import defaultdict
+from itertools import count
 import json
 import numpy as np
 from scipy.sparse import csr_matrix
 from implicit.als import AlternatingLeastSquares
+import sys
+sys.path.append("../")
+from metrics import normalized_average_precision
 
 with open("data/users.json", "r") as read_file:
     users: list = json.load(read_file)
@@ -54,6 +59,47 @@ model.fit(2 * finalMatrix)
 userid = 0
 ids, scores = model.recommend(userid, finalMatrix[userid], N=10, filter_already_liked_items=False)
 
-print(finalMatrix[userid])
-print(ids)
-print(scores)
+#print(finalMatrix[userid])
+#print(ids)
+#print(scores)
+cnt = defaultdict(int)
+for x in users:
+    for y in x["checks"]:
+        for z in y:
+            params = z.split(";")
+            index = products.index(
+                {
+                    "name": params[0],
+                    "cost": int(params[1]),
+                    "merchantName": params[2]
+                }
+            )
+            cnt[index]+=1
+
+indexTopList = sorted(list(cnt.keys()), key = lambda x: -cnt[x])
+nameTopList=[]
+text = ""
+for x in indexTopList:
+    #text += str(x)+" "+str(cnt[x])+" "+products[x]['name']+"\n"
+    nameTopList.append(products[x]['name'])
+
+scores = []
+for x in users:
+    testing = []
+    for y in range(3):
+        for z in x['checks'][-y]:
+            params = z.split(";")
+            index = products.index(
+                {
+                    "name": params[0],
+                    "cost": int(params[1]),
+                    "merchantName": params[2]
+                }
+            )
+            testing.append(index)
+    print(len(testing))
+    metr = normalized_average_precision(testing, indexTopList[:30])
+    scores.append(metr)
+
+print(np.mean(scores))
+#0.04157494615140849
